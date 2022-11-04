fe311860b84d1c9b079e86e94f6524c808d1529c
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-04 13:58:59 +0100

    Add bonus

---
+++ bonus/js/javascript.js
@@ -0,0 +1,23 @@
+// "Baseline" Password Generator
+
+// Chiedere Name
+let name = prompt("Name");
+
+console.log(name)
+
+// Chiedere Surname
+let surname = prompt("Surname");
+
+console.log(surname)
+// Chiedere Favourite Color
+let color = prompt("Color");
+
+console.log(color)
+// Add a Constant
+const constantNumber = 21;
+
+console.log(constantNumber);
+// Output
+document.getElementById ("box").innerHTML = `Ecco a te ${name} la password che nessun altro ti darà mai:
+${name}${surname}${color}${constantNumber}`
+// DEBUG
